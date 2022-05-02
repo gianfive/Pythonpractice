78474fe30201ec7b35d40fae0203cb5eb3039a69
김영우 <104709402+gianfive@users.noreply.github.com>
2022-05-02 12:01:57 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -1,3 +1,4 @@
+
 #작심삼십일 일일코딩 2022.5.1 시작
 print(5)
 print(-10)
@@ -32,6 +33,62 @@ is_adult = age >=3
 print("우리집 " + animal + "의 이름은" + name + "에요")
 hobby = "공놀이"
 #print(name + "는 " +str(age) + "살이며, " + hobby + "을 아주 좋아해여")
-print(name, "는 ", age, + "살이며, ", hobby, "을 아주 좋아해여")
+print("우리집", animal, "의 이름은", name, "에요", name, "는", age, "살이고, 취미는", hobby, "이에요")
 print(name + "는 어른일까요? " + str(is_adult))
 #작심삼십일 일일코딩 2022.5.1 끝
+
+#작심삼십일 일일코딩 2022.5.2 (월) 시작
+station = "인천공항"
+print(station + "행 열차가 들어오고 있습니다.")
+
+print(1+1) #2
+print(3-1) #1
+print(5*2) #10
+print(6/2) #2
+
+print(2**3) #2^3 = 8
+print(5%3) #나머지 2
+print(10%3) #1
+print(5//3) #몫1
+print (10//3) #3
+
+print(10>3) # True
+print(4 >= 7) #False
+print(10 <3) #False
+print(5<=5) #True
+
+print(3 == 3) #True
+print(4 == 2) #False
+print(3+4==7) #True
+print(1 !=3) # 은 앞뒤가 같지 않다. True
+print(not(1 !=3)) # False
+
+print((3>0) and (3<5)) # True
+print((3>0) & (3<5)) # True
+
+print((3>0) or (3>5)) # True
+
+
+print(5>4>3) # True
+print(5>4>7) # false
+
+print(2+3*4) #14
+print((2+3)*4) #20
+number = 2+3*4 #14
+print(number)
+number = number + 2 #16
+print(number)
+number += 2 #18 +=는 number +2 와 같은 의미임
+print(number)
+
+number *=2 #36
+print(number)
+number /=2 #18
+print(number)
+number -= 2 #16
+print(number)
+
+number %=5 #0 (나머지)
+print(number)
+
+#작심삼십일 일일코딩 2022.5.2 (월) 끝
