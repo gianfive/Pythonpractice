fdf994c6c92690fbefc8c6f14ebf6834bc837306
김영우 <104709402+gianfive@users.noreply.github.com>
2022-05-03 14:14:25 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -92,3 +92,53 @@ number %=5 #0 (나머지)
 print(number)
 
 #작심삼십일 일일코딩 2022.5.2 (월) 끝
+
+#작심삼십일 일일코딩 2022.5.3 (화) 시작
+
+print(abs(-5)) #5
+print(pow(4,2)) #4^2 = 4*4=16
+print(max(5,12)) #12
+print(min(5,12)) #5
+print(round(3.14)) #3
+print(round(4.99)) #5
+
+from math import *
+print(floor(4.99)) #내림
+print(ceil(3.14)) # dhffla. 4
+print(sqrt(16)) # 제곱근. 4
+
+from random import *
+
+print(random())  #0.0 ~ 1.1 미만의 임의의 값 생성
+print(random()*10) #0.0 ~10.0 미만의 임의의 값 생성
+print(int(random()*10)) #0~10 미만의 임의의 값 생성
+print(int(random()*10)) #0~10 미만의 임의의 값 생성
+print(int(random()*10)) #0~10 미만의 임의의 값 생성
+print(int(random()*10)+1) #1~10이하의 임의의 값 생성
+print(int(random()*10)+1) #1~10이하의 임의의 값 생성
+print(int(random()*10)+1) #1~10이하의 임의의 값 생성
+print(int(random()*10)+1) #1~10이하의 임의의 값 생성
+print(int(random()*10)+1) #1~10이하의 임의의 값 생성
+print(int(random()*45+1)) #1~45 이하의 임의의 값 생성
+print(int(random()*45+1)) #1~45 이하의 임의의 값 생성
+print(int(random()*45+1)) #1~45 이하의 임의의 값 생성
+print(int(random()*45+1)) #1~45 이하의 임의의 값 생성
+print(int(random()*45+1)) #1~45 이하의 임의의 값 생성
+print(int(random()*45+1)) #1~45 이하의 임의의 값 생성
+
+print(randrange(1,46)) #  1~ 46 이하의 임의의 값 생성
+print(randrange(1,46)) #  1~ 46 이하의 임의의 값 생성
+print(randrange(1,46)) #  1~ 46 이하의 임의의 값 생성
+print(randrange(1,46)) #  1~ 46 이하의 임의의 값 생성
+print(randrange(1,46)) #  1~ 46 이하의 임의의 값 생성
+print(randrange(1,46)) #  1~ 46 이하의 임의의 값 생성
+print(randrange(1,46)) #  1~ 46 이하의 임의의 값 생성
+
+print(randint(1,45)) #1~45 이하의 임의으 값 생성
+print(randint(1,45)) #1~45 이하의 임의으 값 생성
+print(randint(1,45)) #1~45 이하의 임의으 값 생성
+print(randint(1,45)) #1~45 이하의 임의으 값 생성
+print(randint(1,45)) #1~45 이하의 임의으 값 생성
+print(randint(1,45)) #1~45 이하의 임의으 값 생성
+
+#작심삼십일 일일코딩 2022.5.3 (화) 끝
