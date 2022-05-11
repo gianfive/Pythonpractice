bfdfa6d4be49d7306e22e0520c20573e7a9fc20e
김영우 <104709402+gianfive@users.noreply.github.com>
2022-05-11 10:17:19 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -422,3 +422,48 @@ num_list.extend(mix_list)
 print(num_list)
 
 #작심삼십일 일일코딩 2022.5.10 (화) 끝.
+
+
+#작심삼십일 일일코딩 2022.5.11 (수) 시작
+
+# 사전
+
+cabinet = {3:"유재석", 100:"김태호"}
+print(cabinet[3])
+print(cabinet[100])
+
+print(cabinet.get(3))
+# print(cabinet[5]) 오류가 남
+print(cabinet.get(5, "사용가능"))
+print("hi")
+
+print(3 in cabinet) # true
+print(5 in cabinet) # false
+cabinet = {"A-3":"유재석", "B-100":"김태호"}
+print(cabinet["A-3"])
+print(cabinet["B-100"])
+
+#새손님
+print(cabinet)
+cabinet["A-3"]="김종국"
+cabinet["C-20"]="조세호"
+print(cabinet)
+
+# 간 손님
+del cabinet["A-3"]
+print(cabinet)
+
+# key 들만 춢력
+print(cabinet.keys())
+
+# value 들만 출력
+print(cabinet.values())
+
+# key, value 쌍으로 출력
+print(cabinet.items())
+
+# 목욕탕 폐점
+cabinet.clear()
+print(cabinet)
+
+#작심삼십일 일일코딩 2022.5.11 (수) 끝.
