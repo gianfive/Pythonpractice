8595fac57b57dfb84b468f6934cb3f0393698277
김영우 <104709402+gianfive@users.noreply.github.com>
2022-05-12 13:49:42 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -467,3 +467,24 @@ cabinet.clear()
 print(cabinet)
 
 #작심삼십일 일일코딩 2022.5.11 (수) 끝.
+
+#작심삼십일 일일코딩 2022.5.12 (목) 시작
+
+#튜플
+
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
+
+#menu.add("생선까스")
+
+# name = "김종국"
+# age = 20
+# hobby = "코딩"
+# print(name, age, hobby)
+
+(name, age, hobby) = ("김종국", 20, "코딩")
+
+print (name, age, hobby)
+
+#작심삼십일 일일코딩 2022.5.12 (목) 끝.
