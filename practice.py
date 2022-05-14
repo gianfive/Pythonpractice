517e762ada425ccfe3d4c2892c145a69b69ba9e8
김영우 <104709402+gianfive@users.noreply.github.com>
2022-05-14 13:42:22 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -488,3 +488,40 @@ print(menu[1])
 print (name, age, hobby)
 
 #작심삼십일 일일코딩 2022.5.12 (목) 끝.
+
+#작심삼십일 일일코딩 2022.5.14 (토) 시작
+
+# 집합 (set)
+# 중복안됨, 순서없음
+
+my_set = {1,2,3,3,3}
+print(my_set)
+
+java = {"유재석","김태호", "양세형"}
+python = set(["유재석", "박명수"])
+
+#교집합 (java 와 python 을 모두 할 수 있는 개발자)
+
+print(java & python)
+print(java.intersection(python))
+
+
+# 합집합 (java 할 수 있거나 python 할 수 있는 개발자)
+print (java | python)
+print(java.union(python))
+
+
+#차집합 (java 할 수 있지만 python 은 할 줄 모르는 개발자)
+print(java - python)
+print(java.difference(python))
+
+# python 할 수 있는 사람이 늘어남
+python.add("김태호")
+print(python)
+
+# java 까먹음
+
+java.remove("김태호")
+print(java)
+
+#작심삼십일 일일코딩 2022.5.14 (토) 끝.
