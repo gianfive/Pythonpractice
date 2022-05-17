d8adb90dadc327184c6c6927b57f8032bdb7c621
김영우 <104709402+gianfive@users.noreply.github.com>
2022-05-17 11:41:16 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -596,3 +596,27 @@ print("-- 축하합니다 --")
 
 #작심삼십일 일일코딩 2022.5.16 (월) 끝.
 
+#작심삼십일 일일코딩 2022.5.17 (화) 시작
+
+#if
+
+weather = input("오늘 날씨는 어때요? ")
+if weather == "비" or weather == "눈": 
+    print("우산을 챙기세요")
+elif weather == "미세먼지" :
+    print("마스크를 챙기세요")
+else:
+    print("준비물 필요 없어요.")
+
+temp = int(input("기온은 어때요?"))
+
+if 30<= temp:
+    print("너무 더워요. 나가지 마세요")
+elif 10<= temp and temp < 30:
+    print("괜찮은 날씨에요")
+elif 0<= temp < 10:
+    print("외투를 챙기세요")
+else:
+    print("너무 추워요, 나가지 마세요")
+
+#작심삼십일 일일코딩 2022.5.17 (화) 끝.
