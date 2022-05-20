77f8dfef0def4d14732ad46ddfd564c9011e7dae
김영우 <104709402+gianfive@users.noreply.github.com>
2022-05-20 17:51:21 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -649,3 +649,29 @@ for waiting_no in [1,2,3,4,5]: #waiting_no 에 순서대로 1, 2, 3, 4, 5가 차
 # # print("대기번호 : 4")
 
 #작심삼십일 일일코딩 2022.5.18 (수) 끝.
+
+#작심삼십일 일일코딩 2022.5.20 (금) 시작
+
+# while 문 : 어떤 조건이 만족할 때까지 반복하라!
+
+customer = "토르"
+index = 5
+while index >= 1:
+    print("{0}, 커피가 준비 되었습니다. {1} 번 남았어요.".format(customer, index))
+    index -=1
+    if index == 0:
+        print("커피는 폐기처분 되었습니다.")
+
+# customer = "아이언맨"
+# while True:
+#     print("{0}, 커피가 준비되었습니다. 호출 {1} 회".format(customer, index))
+#     index +=1 # 무한루프
+
+customer = "토르"
+person = "Unknown"
+
+while person !=customer :
+    print("{0}, 커피가 준비되었습니다".format(customer))
+    person = input("이름이 어떻게 되세요?")
+
+#작심삼십일 일일코딩 2022.5.20 (금) 끝.
