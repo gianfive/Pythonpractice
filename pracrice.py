2881ef6b9e102aa603d3dcb18f3b66a73528829e
김영우 <104709402+gianfive@users.noreply.github.com>
2022-05-23 11:21:08 +0900

    Add files via upload

---
+++ pracrice.py
@@ -0,0 +1,22 @@
+#작심삼십일 일일코딩 2022.5.23 시작
+
+# 한 줄 for 문
+
+# 출석번호가 1, 2, 3, 4 앞에 100을 붙이기로 함
+
+# students = [1, 2, 3, 4, 5]
+# print(students)
+# students = [i+100 for i in students]
+# print(students)
+
+# # 학생 이름을 길이로 변환
+# students = ["Iron man", "Thor", "I am groot"]
+# students = [len(i) for i in students]
+# print(students)
+
+# 학생 이름을 대문자로 변환
+# students = ["Iron Man", "Thor", "I am groot"]
+# students = [p. upper() for p in students]
+# print(students)
+
+#작심삼십일 일일코딩 2022.5.23 끝.
