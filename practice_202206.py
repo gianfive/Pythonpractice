fd06ca2f79be1a1d4e8a3a19fe51cfe54c923625
김영우 <104709402+gianfive@users.noreply.github.com>
2022-06-01 21:05:24 +0900

    Add files via upload

---
+++ practice_202206.py
@@ -0,0 +1,13 @@
+# 작심삼일 일일코딩 2022.6.1 (수) 시작
+
+# 키워드값
+
+def profile(name, age, main_lang):
+    print(name, age, main_lang)
+
+profile(name="유재석", main_lang="파이썬", age = 20)
+profile(main_lang="자바", age = 25, name = "김태호")
+
+
+# 작심삼일 일일코딩 2022.6.1 (수) 끝.
+
