6b02d58bc367bbaff9de216b3c8beb46dd8f2e4a
김영우 <104709402+gianfive@users.noreply.github.com>
2022-06-04 09:43:20 +0900

    Add files via upload

--- practice_202206.py
+++ practice_202206.py
@@ -56,3 +56,39 @@ print("남은 총 : {0}".format(gun))
 # 반환(return)을 통한 전달값 형태의 코드를 많이 씀
 
 # 작심삼일 일일코딩 2022.6.3 (금) 끝.
+
+# 작심삼일 일일코딩 2022.6.4 (토) 시작
+
+# Quiz) 표준 체중을 구하는 프로그램
+
+# * 표준체중 : 각 개인의 키에 적당한 체중
+
+# (성별에 따른 공식)
+# 남자 : 키 (m) x 키(m) x 22
+# 여자 : 키 (m) x 키(m) x 21
+
+# 조건 1 : 표준 체중은 별도의 함수 내에서 계산
+#         *함수명 : std_weight
+#         *전달값 : 키(height), 성별(gender)
+
+# 조건 2 : 표준 체중은 소수점 둘째자리까지 표시
+
+# (출력예제)
+# 키 175 cm 남자의 표준 체중은 67.38kg 입니다.
+
+
+def std_weight(height, gender): # 키는 m 단위 (실수), gender ="남자"/"여자"
+
+    if gender =="남자": 
+        return height * height * 22
+    else:
+        return height * height * 21
+        
+height = 175 # cm
+gender = "남자"
+
+weight = round(std_weight(height / 100, gender), 2)
+
+print("키 {0} cm {1} 표준 체중은 {2} kg 입니다.".format(height,gender, weight))
+
+# 작심삼일 일일코딩 2022.6.4 (토) 끝.
