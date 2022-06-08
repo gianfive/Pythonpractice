751f90f4916cc528f6aa86398b2bdaf0b19b58bf
김영우 <104709402+gianfive@users.noreply.github.com>
2022-06-08 15:51:51 +0900

    Add files via upload

--- practice_202206.py
+++ practice_202206.py
@@ -166,3 +166,41 @@ print("{0:.2f}".format(5/3))
 # score_file.close()
 
 # 작심삼일 일일코딩 2022.6.7 (화) 끝.
+
+# 작심삼일 일일코딩 2022.6.8 (수) 시작
+# 파일 입출력 (계속)
+
+# score_file = open("score.txt", "a", encoding="utf8")
+# score_file.write("과학 : 80")
+# score_file.write("\n코딩 : 100")
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.read())
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.readline(), end="") # 줄별로 읽기 동작을 수행하고 한 줄 읽고 커서는 다음 줄로 이동
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# score_file.close()
+
+#몇 줄인지 모를 때
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# while True:
+#     line = score_file.readline()
+#     if not line:
+#         break
+#     print(line, end="")
+# score_file.close()
+
+score_file = open("score.txt", "r", encoding="utf8")
+lines = score_file.readlines() # 모든 라인을 리스트형태로 저장
+for line in lines:
+    print(line, end="")
+
+score_file.close()
+
+# 작심삼일 일일코딩 2022.6.8 (수) 끝.
