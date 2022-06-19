0a386da94038a637d120f4caf163629334d83b64
김영우 <104709402+gianfive@users.noreply.github.com>
2022-06-19 11:12:32 +0900

    Add files via upload

--- practice_202206.py
+++ practice_202206.py
@@ -371,47 +371,98 @@
 
 # 일반유닛
 
+# class Unit:
+#     def __init__(self, name, hp):
+#         self.name = name
+#         self.hp = hp
+
+# # 공격유닛
+
+# class AttackUnit(Unit):
+#     def __init__(self, name, hp, damage):
+#         # self.name = name
+#         # self.hp = hp
+#         Unit.__init__(self, name, hp)
+#         self.damage = damage
+
+
+#     def attack(self, location):
+#         print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력] {2}]"\
+#             .format(self.name, location, self.damage))
+
+#     def damaged(self, damage):
+#         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+#         self.hp -=damage
+#         print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+#         if self.hp <=0:
+#             print("{0} : 파괴되었습니다.".format(self.name))
+
+
+# # #파이어뱃 : 공격 유닛, 화염방사기
+# # firebat1 = AttackUnit("파이어뱃", 50, 16)
+# # firebat1.attack("5시")
+
+# # #공격 2번 받는다고 가정
+# # firebat1.damaged(25)
+# # firebat1.damaged(25)
+
+# # 작심삼일 일일코딩 2022.6.17 (금) 끝.
+
+# # 작심삼일 일일코딩 2022.6.17 (토) 시작
+# # 다중상속
+
+# # 드랍쉽 : 공중 유닛, 수송기 마린/ 파이어팻 / 탱크 등을 수송. 공격기등 불가
+
+# class Flyable:
+#     def __init__(self, flying_speed):
+#         self.flying_speed = flying_speed
+    
+#     def fly(self, name, location):
+#         print("{0} :{1} 방향으로 날아갑니다. [속도 {2}]"\
+#             .format(name, location, self.flying_speed))
+            
+# # 공중 공격 유닛 클래스
+# class FlyableAttackUnit(AttackUnit, Flyable):
+#     def __init__(self, name, hp, damage, flying_speed):
+#         AttackUnit.__init__(self, name, hp, damage)
+#         Flyable.__init__(self, flying_speed)
+
+# # 발키리 : 공중공격유닛, 한 번에 14발 미사일 발사.
+# valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+# valkyrie.fly(valkyrie.name, "3시")
+
+# 작심삼일 일일코딩 2022.6.17 (토) 끝.
+
+# 작심삼일 일일코딩 2022.6.17 (일) 시작
+# 연산자 오버로딩 : 자식 클래스에서 정의한 매소드를 쓰고 싶을 때
+
+
+# from dataclasses import asdict
+# from fcntl import DN_DELETE
+# from stat import SF_APPEND
+
+
 class Unit:
-    def __init__(self, name, hp):
+    def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
+        self.speed = speed
+        
+    def move(self, ldocation):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]"\
+            .format(self.name, ldocation, self.speed))
+        Unit: Unit
+        
 
 # 공격유닛
 
 class AttackUnit(Unit):
-    def __init__(self, name, hp, damage):
+    def __init__(self, name, hp, speed, damage):
         # self.name = name
         # self.hp = hp
-        Unit.__init__(self, name, hp)
+        Unit.__init__(self, name, hp, speed)
         self.damage = damage
-
-
-    def attack(self, location):
-        print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력] {2}]"\
-            .format(self.name, location, self.damage))
-
-    def damaged(self, damage):
-        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
-        self.hp -=damage
-        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
-        if self.hp <=0:
-            print("{0} : 파괴되었습니다.".format(self.name))
-
-
-# #파이어뱃 : 공격 유닛, 화염방사기
-# firebat1 = AttackUnit("파이어뱃", 50, 16)
-# firebat1.attack("5시")
-
-# #공격 2번 받는다고 가정
-# firebat1.damaged(25)
-# firebat1.damaged(25)
-
-# 작심삼일 일일코딩 2022.6.17 (금) 끝.
-
-# 작심삼일 일일코딩 2022.6.17 (토) 시작
-# 다중상속
-
-# 드랍쉽 : 공중 유닛, 수송기 마린/ 파이어팻 / 탱크 등을 수송. 공격기등 불가
 
 class Flyable:
     def __init__(self, flying_speed):
@@ -424,11 +475,24 @@ class Flyable:
 # 공중 공격 유닛 클래스
 class FlyableAttackUnit(AttackUnit, Flyable):
     def __init__(self, name, hp, damage, flying_speed):
-        AttackUnit.__init__(self, name, hp, damage)
+        AttackUnit.__init__(self, name, hp, 0, damage) # 지상 스피드는 0으로 처리
         Flyable.__init__(self, flying_speed)
 
-# 발키리 : 공중공격유닛, 한 번에 14발 미사일 발사.
-valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
-valkyrie.fly(valkyrie.name, "3시")
+    def move(self, location):
+        print("[공중유닛이동]")
+        self.fly(self.name, location)
 
-# 작심삼일 일일코딩 2022.6.17 (토) 끝.
+
+# 벌쳐 : 지상 유닛, 기동성이 좋음
+
+vulture = AttackUnit("벌쳐", 80, 10, 20)
+
+# 배틀크루저 : 공중유닛, 체력도 굉장히 좋음, 공격력도 좋음
+
+battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+
+vulture.move("11시")
+# battlecruiser.fly(battlecruiser.name, "9시")
+battlecruiser.move("9시")
+
+# 작심삼일 일일코딩 2022.6.17 (일) 끝.
