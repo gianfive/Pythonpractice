8f4146eb849c94bd9407c99cd5a61358b957cfbc
김영우 <104709402+gianfive@users.noreply.github.com>
2022-06-24 09:33:20 +0900

    Add files via upload

--- practice_202206.py
+++ practice_202206.py
@@ -557,172 +557,213 @@
 # 작심삼일 일일코딩 2022.6.22 (수) 시작
 # 스타크래프트 전반전
 
-from random import *
+# from random import *
 
-# 일반유닛
+# # 일반유닛
 
-class Unit:
-    def __init__(self, name, hp, speed):
-        self.name = name
-        self.hp = hp
-        self.speed = speed
-        print("{0} 유닛이 생성되었습니다.".format(name))
+# class Unit:
+#     def __init__(self, name, hp, speed):
+#         self.name = name
+#         self.hp = hp
+#         self.speed = speed
+#         print("{0} 유닛이 생성되었습니다.".format(name))
         
-    def move(self, ldocation):
-        print("[지상 유닛 이동]")
-        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]"\
-            .format(self.name, ldocation, self.speed))
-        Unit: Unit
+#     def move(self, ldocation):
+#         print("[지상 유닛 이동]")
+#         print("{0} : {1} 방향으로 이동합니다. [속도 {2}]"\
+#             .format(self.name, ldocation, self.speed))
+#         Unit: Unit
     
-    def damaged(self, demage):
-        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
-        self.hp -= damage
-        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
-        if self.hp <= 0:
-            print("{0} : 파괴되었습니다.". format(self.name))
+#     def damaged(self, demage):
+#         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+#         self.hp -= damage
+#         print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+#         if self.hp <= 0:
+#             print("{0} : 파괴되었습니다.". format(self.name))
 
         
-# 공격유닛
+# # 공격유닛
 
-class AttackUnit(Unit):
-    def __init__(self, name, hp, speed, damage):
-        Unit.__init__(self, name, hp, speed)
-        self.damage = damage
+# class AttackUnit(Unit):
+#     def __init__(self, name, hp, speed, damage):
+#         Unit.__init__(self, name, hp, speed)
+#         self.damage = damage
 
-    def attack(self, location):
-        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]"\
-            .format(self.name, location, self.damage))
+#     def attack(self, location):
+#         print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]"\
+#             .format(self.name, location, self.damage))
 
-# 마린
-class Marine(AttackUnit):
-    def __init__(self):
-        AttackUnit.__init__(self, "마린", 40, 1, 5)
+# # 마린
+# class Marine(AttackUnit):
+#     def __init__(self):
+#         AttackUnit.__init__(self, "마린", 40, 1, 5)
 
-    # 스팀팩 : 일정 시간 동안 이동 및 공격 속도를 증가, 체력 10 감소
-    def stimpack(self):
-        if self.hp > 10:
-            self.hp -= 10
-            print("{0} : 스팀팩을 사용합니다. (hp 10 감소)".format(self.name))
-        else:
-            print("{0} : 체력이 부족하여 스팀팩을 사용하지 않습니다.".format(self.name))
+#     # 스팀팩 : 일정 시간 동안 이동 및 공격 속도를 증가, 체력 10 감소
+#     def stimpack(self):
+#         if self.hp > 10:
+#             self.hp -= 10
+#             print("{0} : 스팀팩을 사용합니다. (hp 10 감소)".format(self.name))
+#         else:
+#             print("{0} : 체력이 부족하여 스팀팩을 사용하지 않습니다.".format(self.name))
 
-#탱크
-class Tank(AttackUnit):
-    # 시즈모드 : 탱크를 지상에 고정시켜 더 높은 공격력, 이동불가
-    seize_developed = False # 시즈모드 개발여부
+# #탱크
+# class Tank(AttackUnit):
+#     # 시즈모드 : 탱크를 지상에 고정시켜 더 높은 공격력, 이동불가
+#     seize_developed = False # 시즈모드 개발여부
 
-    def __init__(self):
-        AttackUnit.__init__(self, "탱크", 150, 1, 35)
-        self.seize_mode = False
+#     def __init__(self):
+#         AttackUnit.__init__(self, "탱크", 150, 1, 35)
+#         self.seize_mode = False
 
-    def set_seize_mode(self):
-        if Tank.seize_developed == False:
-            return
+#     def set_seize_mode(self):
+#         if Tank.seize_developed == False:
+#             return
         
-        # 현재 시즈모드가 아닐 때 -> 시즈모드
-        if self.seize_mode == False:
-            print("{0} : 시즈모드로 전환합니다.".format(self.name))
-            self.damage *= 2
-            self.seize_mode = True
+#         # 현재 시즈모드가 아닐 때 -> 시즈모드
+#         if self.seize_mode == False:
+#             print("{0} : 시즈모드로 전환합니다.".format(self.name))
+#             self.damage *= 2
+#             self.seize_mode = True
         
-        # 현재 시즈모드 일 때 -> 시즈모드 해제
-        else: 
-            print("{0} : 시즈모드를 해제합니다.".format(self.name))
-            self.damage /= 2
-            self.seize_mode = False
+#         # 현재 시즈모드 일 때 -> 시즈모드 해제
+#         else: 
+#             print("{0} : 시즈모드를 해제합니다.".format(self.name))
+#             self.damage /= 2
+#             self.seize_mode = False
             
             
-# # 공중 공격 유닛 클래스
-class FlyableAttackUnit(AttackUnit, Flyable):
-    def __init__(self, name, hp, damage, flying_speed):
-        AttackUnit.__init__(self, name, hp, 0, damage) # 지상 스피드는 0으로 처리
-        Flyable.__init__(self, flying_speed)
+# # # 공중 공격 유닛 클래스
+# class FlyableAttackUnit(AttackUnit, Flyable):
+#     def __init__(self, name, hp, damage, flying_speed):
+#         AttackUnit.__init__(self, name, hp, 0, damage) # 지상 스피드는 0으로 처리
+#         Flyable.__init__(self, flying_speed)
 
-    def move(self, location):
-        print("[공중유닛이동]")
-        self.fly(self.name, location)
+#     def move(self, location):
+#         print("[공중유닛이동]")
+#         self.fly(self.name, location)
 
-# 레이스
-class Wraith(FlyableAttackUnit):
-    def __init__(self):
-        FlyableAttackUnit.__init__(self, "레이스", 80, 20, 5)
-        self.clocked = False # 클로킹 모드 (해제 상태)
+# # 레이스
+# class Wraith(FlyableAttackUnit):
+#     def __init__(self):
+#         FlyableAttackUnit.__init__(self, "레이스", 80, 20, 5)
+#         self.clocked = False # 클로킹 모드 (해제 상태)
 
-    def clocking(self):
-        if self.clocked == True: # 클로킹 모드이므로 해제
-            print("{0} : 클로킹 모드를 해제합니다.".format(self.name))
-            self.clocked = False
-        else : # 클로킹 모드 해제이므로 모드설정
-            print("{0} : 클로킹 모드를 설정합니다.".format(self.name))
-            self.clocked = True 
+#     def clocking(self):
+#         if self.clocked == True: # 클로킹 모드이므로 해제
+#             print("{0} : 클로킹 모드를 해제합니다.".format(self.name))
+#             self.clocked = False
+#         else : # 클로킹 모드 해제이므로 모드설정
+#             print("{0} : 클로킹 모드를 설정합니다.".format(self.name))
+#             self.clocked = True 
 
-# 작심삼일 일일코딩 2022.6.22 (수) 끝.
+# # 작심삼일 일일코딩 2022.6.22 (수) 끝.
 
-# 작심삼일 일일코딩 2022.6.23 (목) 시작
-# 스타크래프트 후반전
+# # 작심삼일 일일코딩 2022.6.23 (목) 시작
+# # 스타크래프트 후반전
 
-def game_start():
-    print("[알림] ㅅ ㅐ로운 게임을 시작합니다.")
+# def game_start():
+#     print("[알림] ㅅ ㅐ로운 게임을 시작합니다.")
 
-def game_over():
-    print("Player : gg")
-    print("[Player] 님이 게임을 퇴장하셨습니다.")
+# def game_over():
+#     print("Player : gg")
+#     print("[Player] 님이 게임을 퇴장하셨습니다.")
 
-# 실제 게임 시작
+# # 실제 게임 시작
 
-game_start()
+# game_start()
 
-m1 = Marine()
-m2 = Marine()
-m3 = Marine()
+# m1 = Marine()
+# m2 = Marine()
+# m3 = Marine()
 
-# 탱크 2기 생성
-t1 = Tank()
-t2 = Tank()
+# # 탱크 2기 생성
+# t1 = Tank()
+# t2 = Tank()
 
-# 레이스 1기
-w1 = Wraith()
+# # 레이스 1기
+# w1 = Wraith()
 
-# 유닛 일괄 괄니 (생성된 모든 유닛 append)
+# # 유닛 일괄 괄니 (생성된 모든 유닛 append)
 
-attak_units = []
+# attak_units = []
 
-attak_units.append(m1)
-attak_units.append(m2)
-attak_units.append(m3)
-attak_units.append(t1)
-attak_units.append(t2)
-attak_units.append(w1)
+# attak_units.append(m1)
+# attak_units.append(m2)
+# attak_units.append(m3)
+# attak_units.append(t1)
+# attak_units.append(t2)
+# attak_units.append(w1)
 
-# 전군이동
+# # 전군이동
 
-for unit in attack_units:
-    unit.move("1시")
+# for unit in attack_units:
+#     unit.move("1시")
 
-# 탱크 시즈모드 개발
-Tank.seize_developed = True
-print("[알림] 탱크 시즈모드 개발이 완료되었습니다.")
+# # 탱크 시즈모드 개발
+# Tank.seize_developed = True
+# print("[알림] 탱크 시즈모드 개발이 완료되었습니다.")
 
-# 공격 모드 준비 
-for unit in attack_units:
-    if isinstance(unit, Marine):
-        unit.stimpack()
-    elif isinstance(unit, Tank):
-        unit.set_seize_mode()
-    elif isinstance(unit, Wraith):
-        unit.clocking()
+# # 공격 모드 준비 
+# for unit in attack_units:
+#     if isinstance(unit, Marine):
+#         unit.stimpack()
+#     elif isinstance(unit, Tank):
+#         unit.set_seize_mode()
+#     elif isinstance(unit, Wraith):
+#         unit.clocking()
 
-# 전군 공격
-for unit in attack_units:
-    unit.attack("1시")
+# # 전군 공격
+# for unit in attack_units:
+#     unit.attack("1시")
 
-# 전군 피해
-for unit in attack_units:
-    unit.damaged(randint(5, 21)) # 공격은 랜덤으로 받음 (5~20)
+# # 전군 피해
+# for unit in attack_units:
+#     unit.damaged(randint(5, 21)) # 공격은 랜덤으로 받음 (5~20)
 
-# 게임종료
-game_over()
+# # 게임종료
+# game_over()
 
 # 작심삼일 일일코딩 2022.6.23 (목) 끝.
+
+# 작심삼일 일일코딩 2022.6.24 (금) 시작
+
+# Quiz) 주어진 코드를 활용하여 부동산 프로그램을 작성하시오.quit
+
+# (출력예제)
+
+# 총 3대의 매물이 있습니다.
+# 강남 아파트 매매 10억 2010년
+# 마포 오피스텔 전세 5억 2007년
+# 송파 빌라 월세 500/50 2000년
+
+# [코드]
+class House:
+    #매물 초기화
+    def __init__(self, location, house_type, deal_type, price, completion_year):
+        self.location = location
+        self.house_type=house_type
+        self.deal_type=deal_type
+        self.price=price
+        self.completion_year=completion_year
+
+    #매물 정보 표시
+    def show_detail(self):
+        print(self.location, self.house_type, self.deal_type\
+            , self.price, self.completion_year)
+
+houses = []
+house1 = House("강남", "아파트", "매매", "10억", "2010년")
+house2 = House("마포", "오피스텔", "전세", "5억", "2007년")
+house3 = House("송파", "빌라", "월세", "500/50", "2000년")
+
+houses.append(house1)
+houses.append(house2)
+houses.append(house3)
+
+print("총 {0} 대의 매물이 있습니다.".format(len(houses)))
+for house in houses:
+    house.show_detail()
+
+# 작심삼일 일일코딩 2022.6.24 (금) 끝.
 
 
