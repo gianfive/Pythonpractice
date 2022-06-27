0b2cc50537aa1bf424ca7ccbcfe4a74644a29912
김영우 <104709402+gianfive@users.noreply.github.com>
2022-06-27 11:30:17 +0900

    Add files via upload

--- practice_202206.py
+++ practice_202206.py
@@ -791,18 +791,48 @@
 
 # 작심삼일 일일코딩 2022.6.26 (일) 시작
 
+# try:
+#     print("한자리 전용 계산기")
+#     num1 = int(input("첫째자리숫자입력"))
+#     num2 = int(input("둘째자리숫자입력"))
+#     if num1 >=10 or num2 >=10:
+#         raise ValueError
+#     print("{0} / {1} = {2}".format(num1, num2, int(num1/num2)))
+
+# except ValueError:
+#     print("잘못된 값을 입력하였습니다. 한자리 숫자만 입력하세요")
+ 
+# 작심삼일 일일코딩 2022.6.26 (일) 끝.
+
+# 작심삼일 일일코딩 2022.6.27 (월) 시작
+# 사용자 정의 에러처리
+
+from socket import MsgFlag
+
+
+class BigNumberError(Exception):
+    def __init(self, msg):
+        self.msg = msg
+    
+    def __str__(self):
+            return self.msg
+
 try:
     print("한자리 전용 계산기")
-    num1 = int(input("첫째자리숫자입력"))
-    num2 = int(input("둘째자리숫자입력"))
+    num1 = int(input("첫째자리숫자입력:"))
+    num2 = int(input("둘째자리숫자입력:"))
     if num1 >=10 or num2 >=10:
-        raise ValueError
+        raise BigNumberError("입력값 : {0}, {1}".format(num1, num2))
+
     print("{0} / {1} = {2}".format(num1, num2, int(num1/num2)))
 
 except ValueError:
     print("잘못된 값을 입력하였습니다. 한자리 숫자만 입력하세요")
 
-# 작심삼일 일일코딩 2022.6.26 (일) 끝.
+except BigNumberError as err:
+    print("에러가 발생하였습니다.. 한자리 숫자만 입력하세요")
+    print(err)
 
+# 작심삼일 일일코딩 2022.6.27 (월) 끝.
 
 
