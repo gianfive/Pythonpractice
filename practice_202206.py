7c917ecdd1edfc76f8aa559f0a97d6f11d155824
김영우 <104709402+gianfive@users.noreply.github.com>
2022-06-29 15:26:54 +0900

    Add files via upload

--- practice_202206.py
+++ practice_202206.py
@@ -807,31 +807,31 @@
 # 작심삼일 일일코딩 2022.6.27 (월) 시작
 # 사용자 정의 에러처리
 
-from socket import MsgFlag
+# from socket import MsgFlag
 
 
-class BigNumberError(Exception):
-    def __init(self, msg):
-        self.msg = msg
+# class BigNumberError(Exception):
+#     def __init(self, msg):
+#         self.msg = msg
     
-    def __str__(self):
-            return self.msg
+#     def __str__(self):
+#             return self.msg
 
-try:
-    print("한자리 전용 계산기")
-    num1 = int(input("첫째자리숫자입력:"))
-    num2 = int(input("둘째자리숫자입력:"))
-    if num1 >=10 or num2 >=10:
-        raise BigNumberError("입력값 : {0}, {1}".format(num1, num2))
+# try:
+#     print("한자리 전용 계산기")
+#     num1 = int(input("첫째자리숫자입력:"))
+#     num2 = int(input("둘째자리숫자입력:"))
+#     if num1 >=10 or num2 >=10:
+#         raise BigNumberError("입력값 : {0}, {1}".format(num1, num2))
 
-    print("{0} / {1} = {2}".format(num1, num2, int(num1/num2)))
+#     print("{0} / {1} = {2}".format(num1, num2, int(num1/num2)))
 
-except ValueError:
-    print("잘못된 값을 입력하였습니다. 한자리 숫자만 입력하세요")
+# except ValueError:
+#     print("잘못된 값을 입력하였습니다. 한자리 숫자만 입력하세요")
 
-except BigNumberError as err:
-    print("에러가 발생하였습니다.. 한자리 숫자만 입력하세요")
-    print(err)
+# except BigNumberError as err:
+#     print("에러가 발생하였습니다.. 한자리 숫자만 입력하세요")
+#     print(err)
 
 # 작심삼일 일일코딩 2022.6.27 (월) 끝.
 
@@ -839,7 +839,54 @@ except BigNumberError as err:
 
 # finally 프로그램이 강제 종료되는 것을 막음으로서 우리 프로그램의 완성도를 높일 수가 있다.
 
-finally:
-    print("계산기를 이용해 주셔서 감사합니다.")
+# finally:
+#     print("계산기를 이용해 주셔서 감사합니다.")
 
+
+# 작심삼일 일일코딩 2022.6.28 (화) 끝.
+
+# 작심삼일 일일코딩 2022.6.29 (수) 시작
+
+# Quiz) 동네에 항상 대기 손님이 있는 맛있는 치킨집
+# 대기 손님의 치킨 요리 시간 줄이고자 자동 주문 시스템
+# 시스템 코드 확인, 적절한 예외처리 구문 넣으시오
+
+# 조건1 : 1보다 작거나 숫자가 아닌 입력값이 들어올 때는 ValueError 로 처리
+#         출력 메시지 : "잘못된 값을 입력하였습니다."
+# 조건2 : 대기 손님이 주문할 수 있는 치킨 총 량은 10마리
+#         치킨 소진시 사용자 정의 에러 [SoldOutError]를 발생시키고 프로그램 종료
+#         출력 메시지 : "재고가 소진되어 더 이상 주문을 받지 않습니다."
+
+# [코드]
+class SoldOutError(Exception):
+    pass
+
+chicken = 10
+waiting = 1 # 홀 안에는 현재 만석, 대기번호 1부터 시작
+while(True):
+    try: 
+        print("[남은 치킨 : {0}]".format(chicken))
+        order = int(input("치킨 몇 마리 주문하시겠습니까?"))
+        if order > chicken: # 남은 치킨보다 주문량이 많을 때
+            print("재료가 부족합니다.")
+        elif order <=0:
+            raise ValueError
+        else:
+            print("[대기번호 {0}], {1} 마리 주문이 완료되었습니다.".format(waiting, order))
+            waiting += 1
+            chicken -= order
+
+        if chicken == 0:
+            raise SoldOutError
+
+    except ValueError:
+        print("잘못된 값을 입력하였습니다.")
+    except SoldOutError:
+        print("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+        break
+
+# 작심삼일 일일코딩 2022.6.29 (수) 끝.
+
+
+    
 
