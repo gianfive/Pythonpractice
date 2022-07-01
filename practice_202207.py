2b6649b73bc7d085646c1a5bf1c0771f981f5b28
김영우 <104709402+gianfive@users.noreply.github.com>
2022-07-01 09:23:14 +0900

    Add files via upload

---
+++ practice_202207.py
@@ -0,0 +1,30 @@
+# 작심 일일 코딩 2022.7.1 (금) 시작.
+# 모듈
+
+# import theater_module
+# theater_module.price(3) #3명이서 영화보러 갔을 때 가격
+# theater_module.price_morning(4)
+# theater_module.price_soldier(5)
+
+# import theater_module as mv
+# mv.price(3)
+# mv.price_morning(4)
+# mv.price_soldier(5)
+
+# from theater_module import *
+# # from random import *
+
+# price(3)
+# price_morning(4)
+# price_soldier(5)
+
+# from theater_module import price, price_morning
+# price(5)
+# price_morning(6)
+# price_soldier(7)
+
+from theater_module import price_soldier as price
+price(5)
+
+# 작심 일일 코딩 2022.7.1 (금) 끝.
+
