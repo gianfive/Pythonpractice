8dfaa912f64aed3c93f98bcaf26c775d1908dbe4
김영우 <104709402+gianfive@users.noreply.github.com>
2022-07-06 21:22:57 +0900

    Add files via upload

--- practice_202207.py
+++ practice_202207.py
@@ -46,8 +46,28 @@
 
 # 작심 일일 코딩 2022.7.4 (월) 시작
 
-from travel import *
-trip_to=thailand.VietnamPackage()
-trip_to.detail()
+# # from travel import *
+# trip_to=thailand.VietnamPackage()
+# trip_to.detail()
 
 # 작심 일일 코딩 2022.7.4 (월) 끝.
+
+# 작심 일일 코딩 2022.7.6 (수) 시작
+
+# 모듈 직접 실행
+
+class ThailandPackage:
+    def detail(self):
+        print("[태국 패키지 3박 5일] 방콕, 파타야 여향 (야시장 투어) 50만원")
+
+if __name__ == "main":
+    print("thailand 모듈을 직접 실행")
+    print("이 문장은 모듈을 직접 실행할 때만 실행돼요")
+    trip_to=ThailandPackage()
+    trip_to.detail()
+
+else:
+    print("Thailand 외부에서 모듈 호출")
+
+# 작심 일일 코딩 2022.7.6 (수) 끝.
+
