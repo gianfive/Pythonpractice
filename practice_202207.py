9986b12447a90d5c50b8eca315d71923cc21bdc8
김영우 <104709402+gianfive@users.noreply.github.com>
2022-07-11 11:01:35 +0900

    Add files via upload

--- practice_202207.py
+++ practice_202207.py
@@ -82,11 +82,35 @@
 # 작심 일일 코딩 2022.7.7 (목) 끝.
 
 # 작심 일일 코딩 2022.7.9 (토) 시작
-# pip install
+# # pip install
 
-from bs4 import BeautifulSoup
-soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
-print(soup.prettify())
+# from bs4 import BeautifulSoup
+# soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
+# print(soup.prettify())
 
-# 작심 일일 코딩 2022.7.9 (토) 시작
+# 작심 일일 코딩 2022.7.9 (토) 끝.
 
+
+# 작심 일일 코딩 2022.7.11 (월) 시작
+# 내장함수
+
+# input : 사용자 입력을 받는 함수
+# language = input("무슨 언어를 좋아하세요?")
+# print("{0}은 좋은 언어입니다.".format(language))
+
+#dir : 아딴 객체를 넘겨줳ㅆ을 떄 그 객체가 어떤 변수 함수 가지고있느지 표시
+
+# print(dir())
+# import random # 외장함수
+# print(dir())
+# import pickle
+# print(dir())
+
+# print(dir(random))
+# lst = [1, 2, 3]
+# print(dir(lst))
+
+# name = "Jim"
+# print(dir(name))
+
+# 작심 일일 코딩 2022.7.11 (월) 끝.
