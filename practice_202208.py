3b7f79d7f01daa8422d5473853d284857efa046e
김영우 <104709402+gianfive@users.noreply.github.com>
2022-08-02 08:09:02 +0900

    Add files via upload

--- practice_202208.py
+++ practice_202208.py
@@ -1,22 +1,40 @@
 # 작심 일일 코딩 2022.8.1 (월) 시작
 # 간단한 수식
 
-# print(2+3*4) #14
-# print((2 + 3) * 4) = #20
+print(2+3*4) #14
+print((2+3)*4) #20
 number = 2+3*4
-print(number)
+print(number) #14
 number=number+2
-print(number)
-number +=2
+print(number) #16
+number +=2 #18
 print(number)
 number *=2
-print(number)
+print(number) #36
 number /=2
-print(number)
+print(number) #18
 number -=2
-print(number)
-
+print(number) #16
 number %=5
-print(number)
+print(number) # 16을 5로 나눈 나머지
 
 # 작심 일일 코딩 2022.8.1 (월) 끝.
+
+# 작심 일일 코딩 2022.8.2 (화) 시작
+# 숫자처리함수
+
+print(abs(-5)) # 5
+print(pow(4,2)) # 4^2
+print(max(5,12)) #5
+print(min(5,12)) #12
+print(round(3.14)) #3
+print(round(4.99)) #5
+
+from math import *
+
+print(floor(4.99)) # 내림. 4
+print(ceil(3.14)) # 올림. 4
+print(sqrt(16)) # 제곱근. 4 
+
+# 작심 일일 코딩 2022.8.2 (화) 끝.
+
