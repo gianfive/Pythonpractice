8d98f5c0a59bdc761dbee3b2eac69d6a23697c8d
김영우 <104709402+gianfive@users.noreply.github.com>
2022-08-01 18:33:32 +0900

    Add files via upload

---
+++ practice_202208.py
@@ -0,0 +1,22 @@
+# 작심 일일 코딩 2022.8.1 (월) 시작
+# 간단한 수식
+
+# print(2+3*4) #14
+# print((2 + 3) * 4) = #20
+number = 2+3*4
+print(number)
+number=number+2
+print(number)
+number +=2
+print(number)
+number *=2
+print(number)
+number /=2
+print(number)
+number -=2
+print(number)
+
+number %=5
+print(number)
+
+# 작심 일일 코딩 2022.8.1 (월) 끝.
