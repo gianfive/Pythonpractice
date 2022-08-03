1ea0c6481fd1dd0f04ff122cb4e30c2329d4cbdb
김영우 <104709402+gianfive@users.noreply.github.com>
2022-08-03 20:48:00 +0900

    Add files via upload

--- practice_202208.py
+++ practice_202208.py
@@ -1,40 +1,80 @@
 # 작심 일일 코딩 2022.8.1 (월) 시작
 # 간단한 수식
 
-print(2+3*4) #14
-print((2+3)*4) #20
-number = 2+3*4
-print(number) #14
-number=number+2
-print(number) #16
-number +=2 #18
-print(number)
-number *=2
-print(number) #36
-number /=2
-print(number) #18
-number -=2
-print(number) #16
-number %=5
-print(number) # 16을 5로 나눈 나머지
+# print(2+3*4) #14
+# print((2+3)*4) #20
+# number = 2+3*4
+# print(number) #14
+# number=number+2
+# print(number) #16
+# number +=2 #18
+# print(number)
+# number *=2
+# print(number) #36
+# number /=2
+# print(number) #18
+# number -=2
+# print(number) #16
+# number %=5
+# print(number) # 16을 5로 나눈 나머지
 
 # 작심 일일 코딩 2022.8.1 (월) 끝.
 
 # 작심 일일 코딩 2022.8.2 (화) 시작
 # 숫자처리함수
 
-print(abs(-5)) # 5
-print(pow(4,2)) # 4^2
-print(max(5,12)) #5
-print(min(5,12)) #12
-print(round(3.14)) #3
-print(round(4.99)) #5
+# print(abs(-5)) # 5
+# print(pow(4,2)) # 4^2
+# print(max(5,12)) #5
+# print(min(5,12)) #12
+# print(round(3.14)) #3
+# print(round(4.99)) #5
 
-from math import *
+# from math import *
 
-print(floor(4.99)) # 내림. 4
-print(ceil(3.14)) # 올림. 4
-print(sqrt(16)) # 제곱근. 4 
+# print(floor(4.99)) # 내림. 4
+# print(ceil(3.14)) # 올림. 4
+# print(sqrt(16)) # 제곱근. 4 
 
 # 작심 일일 코딩 2022.8.2 (화) 끝.
+
+# 작심 일일 코딩 2022.8.3 (수) 시작
+
+from random import *
+
+# print(random()) # 0.0 ~ 1.0 미만의 임의의 값을 생성
+# print(random() * 10) # 0.0 ~ 10.0 미만의 임의의 값 생성
+# print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+# print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+# print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+# print(int(random() *10)+1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() *10)+1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() *10)+1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() *10)+1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() *10)+1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() *10)+1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() *10)+1) # 1 ~ 10 이하의 임의의 값 생성
+
+# print(int(random() * 45) +1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) +1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) +1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) +1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) +1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) +1) # 1 ~ 45 이하의 임의의 값 생성
+
+# print(randrange(1,46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1,46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1,46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1,46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1,46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1,46)) # 1 ~ 46 미만의 임의의 값 생성
+
+# print(randint(1,45)) #1~45이하의 임의의 값 생성
+# print(randint(1,45)) #1~45이하의 임의의 값 생성
+# print(randint(1,45)) #1~45이하의 임의의 값 생성
+# print(randint(1,45)) #1~45이하의 임의의 값 생성
+# print(randint(1,45)) #1~45이하의 임의의 값 생성
+# print(randint(1,45)) #1~45이하의 임의의 값 생성
+
+# 작심 일일 코딩 2022.8.3 (수) 끝.
 
