18a8eb6bac0b6bd5b8f92bc1ac9eebe68e1df0e4
김영우 <104709402+gianfive@users.noreply.github.com>
2022-08-08 14:27:59 +0900

    Add files via upload

--- practice_202208.py
+++ practice_202208.py
@@ -96,9 +96,24 @@ from random import *
 
 # 작심 일일 코딩 2022.8.6 (토) 시작
 
-from random import *
-date = randint(4,28)
-print("오프라인 스터디 모임 날짜는 매월 " + str(date) + " 일로 선정되었습니다.")
+# from random import *
+# date = randint(4,28)
+# print("오프라인 스터디 모임 날짜는 매월 " + str(date) + " 일로 선정되었습니다.")
 
 
 # 작심 일일 코딩 2022.8.6 (토) 끝.
+
+# 작심 일일 코딩 2022.8.8 (월) 시작
+# 문자열
+
+sentence = '나는 소년입니다.'
+print(sentence)
+sentence2 = '파이썬은 쉬워요'
+print(sentence2)
+sentence3 = """
+나는 소년이고
+파이썬은 쉬워요
+"""
+print(sentence3)
+
+# 작심 일일 코딩 2022.8.8 (월) 끝.
