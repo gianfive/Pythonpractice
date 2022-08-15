6ba20177e74b966e21556f7a5158ec31465ec690
김영우 <104709402+gianfive@users.noreply.github.com>
2022-08-15 09:15:03 +0900

    Add files via upload

--- practice_202208.py
+++ practice_202208.py
@@ -212,3 +212,68 @@ python = "Python is Amazing"
 
 
 # 작심 일일 코딩 2022.8.14 (일) 끝.
+
+# 작심 일일 코딩 2022.8.15 (월) 시작
+# 리스트 []
+
+# 지하철 칸별로 10명, 20명, 30명
+
+# subway1 = 10
+# subway2 = 20
+# subway3 = 30
+
+# subway = [10, 20, 30]
+# print(subway)
+
+# subway = ["유재석", "조세호", "박명수"]
+# print(subway)
+
+# 조세호 어디 타고 있는가?
+# print(subway.index("조세호"))
+
+# 하하가 다음정류장에서 탐
+# subway.append("하하")
+# print(subway)
+
+# 정형돈을 유재석 조세호 사이에 집어넣음
+# subway.insert(1, "정형돈")
+# print(subway)
+
+# 뒤에서부터 뺌
+# print(subway.pop())
+# print(subway)
+
+# print(subway.pop())
+# print(subway)
+
+# print(subway.pop())
+# print(subway)
+
+# 같은 이름 사람이 몇명?
+# subway.append("유재석")
+# print(subway)
+
+# print(subway.count("유재석"))
+
+#정렬
+# num_list=[5,2,4,3,1]
+# num_list.sort()
+# print(num_list)
+
+# 순서뒤집기
+# num_list.reverse()
+# print(num_list)
+
+# 모두지우기
+# num_list.clear()
+# print(num_list)
+
+# num_list=[5,2,4,3,1]
+# mix_list=("조세호",20,True)
+# print(mix_list)
+
+# 두 리스트를 합침
+# num_list.extend(mix_list)
+# print(num_list)
+
+# 작심 일일 코딩 2022.8.15 (월) 끝.
