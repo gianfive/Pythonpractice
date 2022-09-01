b871ebf50540abe7109b786c5f989780aa554416
김영우 <104709402+gianfive@users.noreply.github.com>
2022-09-01 12:20:55 +0900

    Add files via upload

---
+++ practice_202209.py
@@ -0,0 +1,31 @@
+# 작심삼일 일일코딩 2022.9.1 (목)
+# 퀴즈 #4
+
+# from random import *
+# lst = [1,2,3,4,5]
+# print(lst)
+# shuffle(lst)
+# print(lst)
+# print(sample(lst,1))
+
+### print(int(random() *10)+1) # 1 ~ 10 이하의 임의의 값 생성
+
+# from random import *
+# list = int(random() *10)+1
+# print(list)
+
+# from random import *
+# users = range(1,21) # 1부터 21 직전까지 숫자를 생성
+# # print(type(list))
+# users = list(users)
+# # print(type(user))
+
+# print(users)
+# shuffle(users)
+# print(users)
+
+# winners = sample(users,4)
+
+# print("치킨 : {0}".format(winners[0]))
+# print("커피 : {0}".format(winners[1:]))
+
