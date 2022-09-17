d1ce1822f781c421420f16272061e162416308fd
김영우 <104709402+gianfive@users.noreply.github.com>
2022-09-17 12:21:23 +0900

    Add files via upload

--- practice_202209.py
+++ practice_202209.py
@@ -205,33 +205,52 @@ def open_account():
 # 작심삼일 일일코딩 2022.9.16 금 시작
 
 #입금
-def open_account():
-    print("새로운 계좌가 생성되었습니다.")
+# def open_account():
+#     print("새로운 계좌가 생성되었습니다.")
 
-def deposit(balance, money):
-    print("입금이 완료되었습니다. 잔액은 {0} 원 입니다.".format(balance + money))
-    return balance + money
+# def deposit(balance, money):
+#     print("입금이 완료되었습니다. 잔액은 {0} 원 입니다.".format(balance + money))
+#     return balance + money
 
-#출금
-def withdraw(balance, money):
-    if balance >= money:
-        print("출금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance-money))
-        return balance - money
-    else:
-        print("출금이 완료되지 않았습니다. 잔액은 {0} 원 입니다.".format(balance))
-        return balance
+# #출금
+# def withdraw(balance, money):
+#     if balance >= money:
+#         print("출금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance-money))
+#         return balance - money
+#     else:
+#         print("출금이 완료되지 않았습니다. 잔액은 {0} 원 입니다.".format(balance))
+#         return balance
 
-#출금 수수료
-def withdraw_night(balance, money):
-    commission = 100
-    return commission, balance-money-commission
+# #출금 수수료
+# def withdraw_night(balance, money):
+#     commission = 100
+#     return commission, balance-money-commission
 
-balance = 0
-balance = deposit(balance, 1000)
-# balance = withdraw(balance, 2000)
-# balance = withdraw(balance, 500)
-commission, balance = withdraw_night(balance, 500)
-print("수수료는 {0} 원이며, 잔액은 {1} 원 입니다.".format(commission, balance))
-
+# balance = 0
+# balance = deposit(balance, 1000)
+# # balance = withdraw(balance, 2000)
+# # balance = withdraw(balance, 500)
+# commission, balance = withdraw_night(balance, 500)
+# print("수수료는 {0} 원이며, 잔액은 {1} 원 입니다.".format(commission, balance))
 
 # 작심삼일 일일코딩 2022.9.16 금 끝.
+
+# 작심삼일 일일코딩 2022.9.17 토 시작
+# 기본값
+# def profile(name, age, main_lang):
+#     print("이름: {0}\t나이: {1}\t주 사용언어: {2}" \
+#         .format(name, age, main_lang))
+
+# profile("유재석", 20, "Python")
+# profile("김태호", 25, "JAVA")
+
+# 같은학교 같은학년 같은반 같은 수업
+    
+def profile(name, age=17, main_lang="python"):
+    print("이름: {0}\t나이: {1}\t주 사용언어: {2}" \
+        .format(name, age, main_lang))
+
+profile("유재석")
+profile("김태호")
+
+# 작심삼일 일일코딩 2022.9.17 토 끝.
